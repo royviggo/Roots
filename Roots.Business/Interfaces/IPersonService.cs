89bc8dae63df9476d8f1d34f515f7eeebae5e6fe
royviggo <royviggo@gmail.com>
2020-02-13 19:07:56 +0100

    Updated IPersonService

--- Roots.Business/Interfaces/IPersonService.cs
+++ Roots.Business/Interfaces/IPersonService.cs
@@ -1,7 +1,9 @@
 ﻿using Roots.Business.Filters;
 using Roots.Business.Models;
+using Roots.Business.Requests;
 using Roots.Business.Responses;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Roots.Business.Interfaces
@@ -11,5 +13,8 @@ namespace Roots.Business.Interfaces
         Task<IEnumerable<PersonDto>> GetAllAsync();
         Task<Paged<IEnumerable<PersonDto>>> GetPagedAsync(PersonFilter filter);
         Task<PersonDto> GetByIdAsync(int id);
+        Task<int> Create(PersonCreateRequest request, CancellationToken cancellationToken);
+        Task<bool> Update(PersonUpdateRequest request, CancellationToken cancellationToken);
+        Task<bool> Delete(DeleteRequest request, CancellationToken cancellationToken);
     }
 }
