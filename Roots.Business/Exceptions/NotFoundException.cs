68e7d9906898f6383beb41cc8a03521fc43d43e6
royviggo <royviggo@gmail.com>
2020-02-17 23:02:42 +0100

    Added more constructors

--- Roots.Business/Exceptions/NotFoundException.cs
+++ Roots.Business/Exceptions/NotFoundException.cs
@@ -4,9 +4,11 @@ namespace Roots.Business.Exceptions
 {
     public class NotFoundException : Exception
     {
-        public NotFoundException(object entity, object key)
-            : base($"Entity [{entity.GetType()}] value [{key}] not found.")
-        {
-        }
+        public NotFoundException() : base() { }
+
+        public NotFoundException(object key) : base($"Value [{key}] not found.") { }
+
+        public NotFoundException(object entity, object key) 
+            : base($"Entity [{entity.GetType()}] value [{key}] not found.") {}
     }
 }
