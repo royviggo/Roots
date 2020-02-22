f0cb0f13a0686376b2c83d2ed1cf6b5b315de4e6
royviggo <royviggo@gmail.com>
2020-02-22 20:08:54 +0100

    Added ModifiedDate and on delete cascade

--- Roots.Data.Sql/dbo/Tables/Child.sql
+++ Roots.Data.Sql/dbo/Tables/Child.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[Child]
 (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [FamilyId]     INT            NOT NULL,
-    [PersonId]     INT            NOT NULL, 
-    [CreatedDate]  DATETIME       NULL, 
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [FamilyId]        INT            NOT NULL,
+    [PersonId]        INT            NOT NULL, 
+    [CreatedDate]     DATETIME       NULL, 
+    [ModifiedDate]    DATETIME       NULL,
     CONSTRAINT [PK_Child] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Child_Family] FOREIGN KEY ([FamilyId]) REFERENCES [dbo].[Family] ([Id]),
-    CONSTRAINT [FK_Child_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([Id]),
+    CONSTRAINT [FK_Child_Family] FOREIGN KEY ([FamilyId]) REFERENCES [dbo].[Family] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Child_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([Id]) ON DELETE CASCADE,
 )
