2aec16d0998cb855390ef560d715d975c63b29f9
royviggo <royviggo@gmail.com>
2020-06-23 08:47:01 +0200

    Added database user roots

---
+++ Roots.Data.Sql/Roots.sql
@@ -0,0 +1,13 @@
+﻿CREATE LOGIN [roots] WITH PASSWORD = 'Password123!', DEFAULT_DATABASE=[roots]
+GO
+
+CREATE USER [roots] FOR LOGIN [roots] WITH DEFAULT_SCHEMA = dbo
+GO
+
+GRANT CONNECT TO [roots]
+GO
+
+ALTER ROLE [db_datareader] ADD MEMBER [roots]
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [roots]
+GO
